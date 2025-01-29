67142490bd4976677c55b519f583136025b78a31
Klaas Neirinck <klaas.neirinck@gmail.com>
2025-01-29 22:59:13 +0100

    handle device not being available at startup

--- custom_components/luke_roberts/__init__.py
+++ custom_components/luke_roberts/__init__.py
@@ -2,9 +2,11 @@
 
 from __future__ import annotations
 
+from homeassistant.components import bluetooth
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import DOMAIN, HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
 
 PLATFORMS: list[Platform] = [Platform.LIGHT]
 
@@ -12,6 +14,15 @@ PLATFORMS: list[Platform] = [Platform.LIGHT]
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Luke Roberts Luvo BLE from a config entry."""
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
+
+    ble_device = bluetooth.async_ble_device_from_address(
+        hass, entry.unique_id.upper(), True
+    )
+    if not ble_device:
+        raise ConfigEntryNotReady(
+            f"Unable to find device with address {entry.unique_id}, ensure it's powered on"
+        )
+
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
